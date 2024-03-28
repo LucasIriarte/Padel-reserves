99e70f54b9a2eb80e2df6aa962ad4d4eb7ccec13
LucasIriarte <lucasiriarte67@gmail.com>
2024-03-27 22:13:59 -0300

    change: getBooking is getting the date

--- api/src/controllers/getBooking.js
+++ api/src/controllers/getBooking.js
@@ -1,8 +1,13 @@
 
 
-export const getBooking = (req, res) => {
+export const getBooking = async (req, res) => {
     try {
-        return res.send("entrando en get booging")
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const dateFormated = `${day}/${month}/${year}`
+        return res.send(dateFormated)
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
