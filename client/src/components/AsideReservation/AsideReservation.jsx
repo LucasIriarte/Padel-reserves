87fef7ab14b6ad1dce82ec8150f3d91d7cda68fb
LucasIriarte <lucasiriarte67@gmail.com>
2024-05-02 15:35:02 -0300

    change:validate in form working

--- client/src/components/AsideReservation/AsideReservation.jsx
+++ client/src/components/AsideReservation/AsideReservation.jsx
@@ -1,16 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineCloseSquare } from "react-icons/ai"
 
 
 const AsideReservation = ({ onClose, hourReserve }) => {
-    const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    const [email,setEmail] = useState("")
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    if(validEmail.test(email)){
+    const validName = /^[A-Za-z\s]+$/;
+    const validEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const validPhone = /^\d{10}$/;
+    const [errors, setErrors] = useState({
+        name:"",
+        phone:"",
+        email:"",
 
+    })
+    const [form, setForm] = useState({
+        name: "",
+        phone: "",
+        email: "",
+        dateAppointment:"",
+        hourShift:hourReserve,
+        hourEnd:""
+    })
+    const handleForm = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
     }
+
+    useEffect(()=> {
+        validate()
+        console.log(form)
+    },[form])
+
+    const validate = () => {
+        const newErrors = {}
+        if(validName.test(form.name)){
+            newErrors.name=""
+        }
+        if(!validName.test(form.name) && form.name !== ""){
+            newErrors.name="ahí hay numeros papi"
+        }
+        if(validPhone.test(form.phone)){
+            newErrors.phone=""
+        }
+        if(!validPhone.test(form.phone)){
+            newErrors.phone="tiene que tener 10 nums si o sí papi"
+        }
+        if(validEmail.test(form.email)){
+            newErrors.email = ""
+        }
+        if(!validEmail.test(form.email) && form.email !== ""){
+            newErrors.email = "email no valido papi"
+        }
+        setErrors(newErrors)
+    }
+
     return (
         <div className="fixed top-0 right-0 w-screen h-screen bg-black/[.4] z-20 flex justify-end">
             <div className="bg-white w-4/5">
@@ -21,16 +65,16 @@ const AsideReservation = ({ onClose, hourReserve }) => {
                 <h3 className="pl-8 pt-8 text-2xl">Information reserve</h3>
                 <form action="" className="flex flex-col items-start justify-around h-auto pl-8 pt-4 w-full">
                     <div className="flex flex-col w-full">
-                        <label className="text-xl">Name:</label>
-                        <input type="text" className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="Martín..."/>
+                        <label className="text-xl">Name</label>
+                        <input type="text" onChange={(e) => handleForm(e)} value={form.name} name="name" className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="Martín..." />
                     </div>
                     <div className="flex flex-col w-full pt-5">
-                        <label className="text-xl">Phone:</label>
-                        <input type="text" className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="Alvarez..."/>
+                        <label className="text-xl">Phone</label>
+                        <input type="number" onChange={(e)=>handleForm(e)} value={form.phone} name="phone" className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="1154649853" />
                     </div>
                     <div className="flex flex-col w-full pt-5">
-                        <label className="text-xl">Email:</label>
-                        <input type="text" onChange={(e)=>handleEmail(e)} className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="example@gmail.com"/>
+                        <label className="text-xl">Email (optional)</label>
+                        <input type="text" onChange={(e) => handleForm(e)} value={form.email} name="email" className="w-5/6 h-10 rounded-lg mt-1 border border-primary-5 placeholder:text-gray-300 pl-2 focus:outline-none" placeholder="example@gmail.com" />
                     </div>
                 </form>
                 <div className="pl-8">
