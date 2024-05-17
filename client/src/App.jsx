c9ad292eaafaca8ded5bc6477b3483f9f258d5a5
LucasIriarte <lucasiriarte67@gmail.com>
2024-05-16 21:43:46 -0300

    change & add: ProtectedRoute added & change Route in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx"
 import axios from "axios"
 import LoginPage from "./components/LoginPage/LoginPage.jsx";
+
 axios.defaults.baseURL = "http://localhost:3000"
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<LoginPage />}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={
+        <ProtectedRoute>
+          <Home/>
+        </ProtectedRoute>} />
+    </Routes>
   )
 }
 
