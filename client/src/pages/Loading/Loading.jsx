2e512b65ff1de6b2368dd3e7bf3a86b819361218
LucasIriarte <lucasiriarte67@gmail.com>
2024-07-15 21:03:44 -0300

    change: loading styles

--- client/src/pages/Loading/Loading.jsx
+++ client/src/pages/Loading/Loading.jsx
@@ -2,7 +2,7 @@
 
 const Loading = () => {
     return (
-        <h1>Loading...</h1>
+        <h1 className="text-primary-5 text-center text-2xl font-bold bg-primary-5/[.1] border border-y-primary-4">Loading...</h1>
     )
 }
 
